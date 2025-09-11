272477fbefd02b64fe56faede965484e236976e0
CanalCineflix <haussthii@gmail.com>
2025-09-11 16:01:51 -0300

    Update models.py

--- models.py
+++ models.py
@@ -24,7 +24,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     phone = db.Column(db.String(20), nullable=False)
     restaurant_name = db.Column(db.String(100), nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     whatsapp = db.Column(db.String(20), nullable=True)
     
