840df735ebd83071d2b60069bb54217298fdc1b4
CanalCineflix <haussthii@gmail.com>
2025-09-11 15:24:39 -0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,14 +1,8 @@
-from app import create_app
-from extensions import db
+# extensions.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
-# Criar a instância do aplicativo
-app = create_app()
-
-# Entrar no contexto do aplicativo para garantir que o SQLAlchemy funcione
-with app.app_context():
-    print("Tentando criar todas as tabelas...")
-    try:
-        db.create_all()
-        print("Tabelas criadas com sucesso.")
-    except Exception as e:
-        print(f"Ocorreu um erro ao criar as tabelas: {e}")
+db = SQLAlchemy()
+login_manager = LoginManager()
+migrate = Migrate()
