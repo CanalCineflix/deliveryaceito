75901415ec2562dd4558a0777bf73a4f258008ca
CanalCineflix <haussthii@gmail.com>
2025-09-11 15:39:54 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -6,6 +6,4 @@ set -o errexit
 Install dependencies from requirements.txt
 pip install -r requirements.txt
 
-Create initial migration
-Esse comando cria as tabelas se a migração ainda não existe
 flask db upgrade
