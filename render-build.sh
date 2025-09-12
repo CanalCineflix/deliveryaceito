9309a7ff687c4f996796848e2a4118d1fb4d91dc
CanalCineflix <haussthii@gmail.com>
2025-09-12 13:36:34 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -6,4 +6,9 @@ set -o errexit
 Install dependencies from requirements.txt
 pip install -r requirements.txt
 
+Corrige o problema de inconsistência de histórico de migrações
+e aplica todas as mudanças pendentes ao banco de dados.
+Esta é a melhor prática para ambientes de produção como o Render.
+flask db stamp head
+flask db migrate -m "Sincroniza migrações com o modelo mais recente"
 flask db upgrade
