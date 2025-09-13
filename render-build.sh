e8df7dba63b6230a0647f6b046e1a93fec0c78b7
CanalCineflix <haussthii@gmail.com>
2025-09-13 15:10:44 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,8 +3,14 @@
 # Sair imediatamente se um comando falhar
 set -o errexit
 
-# Entra no diretório 'src' para garantir o caminho correto
+# Entra no diretório 'src'
 cd /opt/render/project/src
 
-# Instala as dependências do projeto listadas em requirements.txt
+# Instala as dependências do projeto usando pip
 pip install -r requirements.txt
+
+# Executa o script de migrações
+python run_migrations.py
+
+# Você pode rodar outros scripts de setup aqui se precisar
+# Exemplo: python create_plans.py
