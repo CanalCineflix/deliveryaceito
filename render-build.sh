be994972e17a1d7fca8418fdf1914ab9bf3bcbc4
CanalCineflix <haussthii@gmail.com>
2025-09-13 15:02:21 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,21 +3,8 @@
 # Sair imediatamente se um comando falhar
 set -o errexit
 
-# Entra no diretório 'src'
+# Entra no diretório 'src' para garantir o caminho correto
 cd /opt/render/project/src
 
-# Instalar as dependências do projeto usando pip
+# Instala as dependências do projeto listadas em requirements.txt
 pip install -r requirements.txt
-
-# Verifica se o arquivo manage.py existe no diretório atual antes de continuar
-if [ ! -f "manage.py" ]; then
-    echo "Erro: O arquivo manage.py não foi encontrado no diretório /opt/render/project/src."
-    echo "Certifique-se de que a estrutura do seu projeto está correta."
-    exit 1
-fi
-
-# Coletar os arquivos estáticos para o Gunicorn
-python manage.py collectstatic --noinput
-
-# Executar as migrações do banco de dados
-python manage.py migrate
