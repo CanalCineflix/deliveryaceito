3ec8d8e0e562f3b06de93709799493261f09c458
CanalCineflix <haussthii@gmail.com>
2025-09-13 15:00:07 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,13 +3,18 @@
 # Sair imediatamente se um comando falhar
 set -o errexit
 
-# Entra no diretório 'src' onde o pyproject.toml deve estar
-# Embora pyproject.toml não exista, é uma boa prática
-# manter o cd para a raiz do projeto.
+# Entra no diretório 'src'
 cd /opt/render/project/src
 
 # Instalar as dependências do projeto usando pip
 pip install -r requirements.txt
+
+# Verifica se o arquivo manage.py existe no diretório atual antes de continuar
+if [ ! -f "manage.py" ]; then
+    echo "Erro: O arquivo manage.py não foi encontrado no diretório /opt/render/project/src."
+    echo "Certifique-se de que a estrutura do seu projeto está correta."
+    exit 1
+fi
 
 # Coletar os arquivos estáticos para o Gunicorn
 python manage.py collectstatic --noinput
