0171e4d81e4b8061e2b4c05b5434c8d44b3c27f1
CanalCineflix <haussthii@gmail.com>
2025-09-13 14:45:24 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-Exit immediately if a command exits with a non-zero status.
+# Sair imediatamente se um comando falhar
 set -o errexit
 
-Install dependencies from requirements.txt
-pip install -r requirements.txt
+# Definir a variável de ambiente para que o Poetry não crie o ambiente virtual
+# dentro do diretório do projeto, pois o Render já faz isso.
+export POETRY_VIRTUALENVS_IN_PROJECT=false
 
-Apply all pending database migrations to the latest revision.
-This is the best practice for production environments.
-flask db upgrade
+# Instalar as dependências do projeto
+poetry install --no-dev
+
+# Coletar os arquivos estáticos para o Gunicorn
+python manage.py collectstatic --noinput
+
+# Executar as migrações do banco de dados
+python manage.py migrate
