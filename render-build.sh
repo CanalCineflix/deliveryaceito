7d4afd34eae83731ed15f48ffafde457d3705050
CanalCineflix <haussthii@gmail.com>
2025-09-14 02:27:42 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,7 +2,9 @@
 set -o errexit
 cd /opt/render/project/src
 pip install -r requirements.txt
+if [ ! -d "migrations" ]; then
 flask db init
+fi
 flask db migrate --autogenerate -m "Aplicação de migrações automáticas"
 flask db upgrade
 python create_plans.py
