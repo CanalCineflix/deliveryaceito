2fcf30f3fc31ccc3e3e61df7ee9479714f4fcd39
CanalCineflix <haussthii@gmail.com>
2025-09-18 15:40:52 -0300

    Update auth_routes.py

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -45,7 +45,7 @@ def register():
             # Login automático após o cadastro
             login_user(new_user)
             # Redirecionamento para a página de escolha de planos
-            return redirect(url_for('planos.choose'))
+            rreturn redirect(url_for('planos.choose_plan'))
             
         except IntegrityError:
             db.session.rollback()
